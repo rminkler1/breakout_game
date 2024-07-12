520be9a97453cad4f3476a9dc5ce38a6e903bae7
robertminkler <robertminkler@gmail.com>
2024-07-11 20:18:16 -0700

    Refactor Default ball speed to remove a magic number. Change random_ball_speeds to RANDOM_BALL_SPEEDS

--- ball.py
+++ ball.py
@@ -1,7 +1,10 @@
 import random
 from turtle import Turtle
 
-random_ball_speeds = (4, 5, 6)
+# Constants
+RANDOM_BALL_SPEEDS = (4, 5, 6)
+DEFAULT_BALL_SPEED = 5
+
 
 
 class Ball(Turtle):
@@ -13,7 +16,7 @@ class Ball(Turtle):
         self.goto(0, ypos)
         self.speed("fastest")
         self.x_move = random.choice((5, -5))
-        self.y_move = 5
+        self.y_move = DEFAULT_BALL_SPEED
         self.move_speed = speed
 
     def move(self):
@@ -24,22 +27,22 @@ class Ball(Turtle):
     def bounce_y(self, overshoot):
         self.y_move *= -1
         if self.y_move < 0:
-            self.y_move = random.choice(random_ball_speeds) * -1
+            self.y_move = random.choice(RANDOM_BALL_SPEEDS) * -1
         else:
-            self.y_move = random.choice(random_ball_speeds)
+            self.y_move = random.choice(RANDOM_BALL_SPEEDS)
         new_y = self.ycor() - overshoot
         self.sety(new_y)
 
     def bounce_x(self, overshoot):
         self.x_move *= -1
         if self.x_move < 0:
-            self.x_move = random.choice(random_ball_speeds) * -1
+            self.x_move = random.choice(RANDOM_BALL_SPEEDS) * -1
         else:
-            self.x_move = random.choice(random_ball_speeds)
+            self.x_move = random.choice(RANDOM_BALL_SPEEDS)
         new_x = self.xcor() - overshoot
         self.setx(new_x)
 
     def reset_pos(self, ypos):
         self.goto(0, ypos)
-        self.y_move = 5
+        self.y_move = DEFAULT_BALL_SPEED
 
