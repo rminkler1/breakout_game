11fd5c75bc112be41bd47b8d1dd02ad931104286
robertminkler <robertminkler@gmail.com>
2025-03-10 10:20:46 -0700

    Refactor Constants into seperate file

--- main.py
+++ main.py
@@ -5,18 +5,8 @@ from ball import Ball
 from bricks import Bricks
 from paddle import Paddle
 from scoreboard import Scoreboard
+from constants import *
 
-# Global Variables
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 600
-BALL_STARTING_SPEED = 0.02  # default 0.02
-BALL_STARTING_YPOS = -200
-PADDLE_STARTING_YPOS = -250
-PADDLE_STRETCH = 10
-GAME_TITLE = "Breakout!"
-BRICK_ROW_COLORS = ["#F4EDCC", "#A4CE95", "#6196A6", "#5F5D9C"]
-WALL_OFFSET = 10
-STARTING_BALL_COUNT = 4
 
 game_over_text = "GAME OVER!"
 rows_of_bricks = [[], [], [], []]
