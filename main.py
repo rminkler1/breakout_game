5e37bda58382a7ecf0a3d9a8fccbba993ce40b97
robertminkler <robertminkler@gmail.com>
2025-04-02 15:58:44 -0700

    Move ball and paddle into game variables. Move brick collisions into method.

--- main.py
+++ main.py
@@ -25,25 +25,26 @@ class Game:
         # place scoreboard
         self.scoreboard = Scoreboard(SCREEN_HEIGHT, STARTING_BALL_COUNT)
 
-    def run(self):
         # place bricks on screen
         self.place_bricks()
 
         # place paddle on screen
-        paddle = Paddle(PADDLE_STARTING_YPOS, SCREEN_WIDTH, PADDLE_STRETCH, screen=self.screen)
+        self.paddle = Paddle(PADDLE_STARTING_YPOS, SCREEN_WIDTH, PADDLE_STRETCH, screen=self.screen)
 
         # place ball on screen
-        ball = Ball(speed=BALL_STARTING_SPEED, ypos=BALL_STARTING_YPOS)
+        self.ball = Ball(speed=BALL_STARTING_SPEED, ypos=BALL_STARTING_YPOS)
 
+
+    def run(self):
         # Change paddle position based on key input
         self.screen.listen()
-        self.screen.onkeypress(paddle.move_right, "Right")
-        self.screen.onkeypress(paddle.move_left, "Left")
+        self.screen.onkeypress(self.paddle.move_right, "Right")
+        self.screen.onkeypress(self.paddle.move_left, "Left")
 
         # Gameplay begins here
         while self.scoreboard.balls > 0:
-            time.sleep(ball.move_speed)  # Slow the game down
-            ball.move()
+            time.sleep(self.ball.move_speed)  # Slow the game down
+            self.ball.move()
 
             """
             Wall and paddle Collision logic
@@ -55,52 +56,37 @@ class Game:
             """
 
             # Detect roof collision
-            if ball.ycor() > SCREEN_TOP and ball.y_move > 0:
-                y_diff = int(ball.ycor() - ((SCREEN_HEIGHT / 2) - WALL_OFFSET))
-                ball.bounce_y(y_diff * 2)
+            if self.ball.ycor() > SCREEN_TOP and self.ball.y_move > 0:
+                y_diff = int(self.ball.ycor() - ((SCREEN_HEIGHT / 2) - WALL_OFFSET))
+                self.ball.bounce_y(y_diff * 2)
 
             # Detect right wall collisions - extra 10 px needed to bounce off wall
-            if ball.xcor() > SCREEN_RIGHT and ball.x_move > 0:
-                x_diff = ball.xcor() - SCREEN_RIGHT
-                ball.bounce_x(x_diff * 2)
+            if self.ball.xcor() > SCREEN_RIGHT and self.ball.x_move > 0:
+                x_diff = self.ball.xcor() - SCREEN_RIGHT
+                self.ball.bounce_x(x_diff * 2)
 
             # Detect left wall collisions
-            if ball.xcor() < SCREEN_LEFT and ball.x_move < 0:
-                x_diff = ball.xcor() - SCREEN_LEFT
-                ball.bounce_x(x_diff * 2)
+            if self.ball.xcor() < SCREEN_LEFT and self.ball.x_move < 0:
+                x_diff = self.ball.xcor() - SCREEN_LEFT
+                self.ball.bounce_x(x_diff * 2)
 
             # Detect collision with paddle top
-            if ball.distance(paddle) < (10 * PADDLE_STRETCH) and ball.ycor() < PADDLE_STARTING_YPOS + 20 and ball.y_move < 0:
-                y_diff = ball.ycor() - (PADDLE_STARTING_YPOS + 20)
+            if self.ball.distance(self.paddle) < (10 * PADDLE_STRETCH) and self.ball.ycor() < PADDLE_STARTING_YPOS + 20 and self.ball.y_move < 0:
+                y_diff = self.ball.ycor() - (PADDLE_STARTING_YPOS + 20)
                 # bounce off sides or top of paddle
                 if y_diff > -10:
-                    ball.bounce_y(y_diff * 2)
+                    self.ball.bounce_y(y_diff * 2)
                 else:
-                    ball.bounce_x(ball.x_move * 3)
+                    self.ball.bounce_x(self.ball.x_move * 3)
 
             # Detect ball hits bottom of screen
-            if ball.ycor() < SCREEN_BOTTOM:
-                ball.reset_pos(BALL_STARTING_YPOS)
-                paddle.reset_pos()
+            if self.ball.ycor() < SCREEN_BOTTOM:
+                self.ball.reset_pos(BALL_STARTING_YPOS)
+                self.paddle.reset_pos()
                 self.scoreboard.balls -= 1
 
-            # Detect brick collision and count remaining bricks to detect end of level
-            remaining_bricks = 0
-            for row in self.rows_of_bricks:
-                remaining_bricks += len(row)
-                for brick in row:
-                    # 43 compensates for the round ball hitting brick corners
-                    if ball.distance(brick) < 43 and brick.ycor() - 20 < ball.ycor() < brick.ycor() + 20:
-                        brick.sety(SCREEN_HEIGHT)  # move the brick off-screen
-                        row.remove(brick)  # delete the brick from memory
-                        self.scoreboard.score += 10
-                        ball.bounce_y(0)
-            if remaining_bricks == 0:
-                self.level += 1
-                paddle_width = max(3, PADDLE_STRETCH - self.level)
-                ball.reset_pos(BALL_STARTING_YPOS)
-                self.place_bricks()
-                paddle.shapesize(stretch_len=paddle_width, stretch_wid=1.0)
+            # handle brick collisions
+            self.brick_collision()
 
             # update scoreboard
             self.scoreboard.draw_scoreboard()
@@ -123,6 +109,25 @@ class Game:
                 x_pos = HALF_SCREEN - (each_brick * BRICK_WIDTH_PX) - 50
                 brick_ = Bricks(BRICK_ROW_COLORS[i], xpos=x_pos, ypos=(i * 30) + 100)
                 self.rows_of_bricks[i].append(brick_)
+
+    def brick_collision(self):
+        # Detect brick collision and count remaining bricks to detect end of level
+        remaining_bricks = 0
+        for row in self.rows_of_bricks:
+            remaining_bricks += len(row)
+            for brick in row:
+                # 43 compensates for the round ball hitting brick corners
+                if self.ball.distance(brick) < 43 and brick.ycor() - 20 < self.ball.ycor() < brick.ycor() + 20:
+                    brick.sety(SCREEN_HEIGHT)  # move the brick off-screen
+                    row.remove(brick)  # delete the brick from memory
+                    self.scoreboard.score += 10
+                    self.ball.bounce_y(0)
+        if remaining_bricks == 0:
+            self.level += 1
+            paddle_width = max(3, PADDLE_STRETCH - self.level)
+            self.ball.reset_pos(BALL_STARTING_YPOS)
+            self.place_bricks()
+            self.paddle.shapesize(stretch_len=paddle_width, stretch_wid=1.0)
 
 if __name__ == '__main__':
     game = Game()
